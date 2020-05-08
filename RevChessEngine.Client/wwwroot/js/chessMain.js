ee682450a0330a430908256d2ca1c9f721a27d96
Randy Steinkamp <randy118@utexas.edu>
2020-05-08 09:33:07 -0500

    change computer player to 'random moves'

---
+++ RevChessEngine.Client/wwwroot/js/chessMain.js
@@ -0,0 +1,67 @@
+console.log("starting chessMain");
+var board = null
+var game = new Chess()
+
+function onDragStart (source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false
+
+  // only pick up pieces for White
+  if (piece.search(/^b/) !== -1) return false
+}
+
+// place position picking algorithm here
+function makeRandomMove () {
+  var possibleMoves = game.moves()
+
+  // game over
+  if (possibleMoves.length === 0) return
+
+  var randomIdx = Math.floor(Math.random() * possibleMoves.length)
+  game.move(possibleMoves[randomIdx])
+  board.position(game.fen())
+}
+
+function clickShowPositionBtn () {
+  console.log('Current position as an Object:')
+  console.log(board.position())
+
+  console.log('Current position as a FEN string:')
+  console.log(board.fen())
+}
+
+
+function onDrop (source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  })
+
+  // illegal move
+  if (move === null) return 'snapback'
+
+  // make random legal move for black
+  window.setTimeout(makeRandomMove, 250)
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd () {
+  board.position(game.fen())
+}
+
+var config = {
+  pieceTheme: '../chessboard/img/chesspieces/wikipedia/{piece}.png',
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+}
+board = Chessboard('myBoard', config)
+
+$('#showPositionBtn').on('click', clickShowPositionBtn)
+
+$('#flipOrientationBtn').on('click', board.flip)
